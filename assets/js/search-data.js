980dd162fb32e6032e15594e4d7864cf91eab8d8
airob-group <hangma@sfu.ca>
2025-11-13 22:24:46 +0000

    Deploying to gh-pages from @ airob-group/airob-group.github.io@72b25c2223b0ef185ee3e8c5644ee2907f47e6a9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -454,11 +454,18 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
+        id: 'social-dblp',
+        title: 'DBLP',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://dblp.org/pid/43/7175-1.html", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%68%61%6E%67%6D%61 %61%74 %73%66%75 %64%6F%74 %63%61", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -468,25 +475,11 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=KJbsVl8AAAAJ", "_blank");
         },
       },{
       id: 'light-theme',

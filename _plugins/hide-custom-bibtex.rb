f16853271b4f79f91a713fc23b23f110f6dc182d
AIRob-group <hangma@sfu.ca>
2026-08-24 19:39:19 +0000

    Update and BibTeX field filtering

--- _plugins/hide-custom-bibtex.rb
+++ _plugins/hide-custom-bibtex.rb
@@ -1,10 +1,11 @@
 module Jekyll
   module HideCustomBibtex
     def hideCustomBibtex(input)
+      input = input.to_s
 	    keywords = @context.registers[:site].config['filtered_bibtex_keywords']
 
 	    keywords.each do |keyword|
-		    input = input.gsub(/^.*\b#{keyword}\b *= *\{.*$\n/, '')
+		    input = input.gsub(/^\s*#{Regexp.escape(keyword.to_s)}\s*=\s*[^\r\n]*\r?\n?/, '')
 	    end
 
       # Clean superscripts in author lists
